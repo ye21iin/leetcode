3271a752375b8f9fe89a79e02db21a4e7055338c
Yelin Kim <97957438+ye21iin@users.noreply.github.com>
2025-05-26 17:23:43 +1000

    Time: 364 ms (97.34%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,5 @@
 SELECT w1.id
 FROM Weather w1
 JOIN Weather w2
-  ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+  ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
 WHERE w1.temperature > w2.temperature;
