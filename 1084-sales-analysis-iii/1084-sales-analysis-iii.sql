54d314ecea55630be92b1647ba056cad3e81980f
Yelin Kim <97957438+ye21iin@users.noreply.github.com>
2026-03-05 23:43:31 -0800

    Time: 1054 ms (84.7%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,8 @@
+SELECT
+    s.product_id,
+    p.product_name
+FROM Sales s
+JOIN Product p ON s.product_id = p.product_id
+GROUP BY s.product_id, p.product_name
+HAVING MIN(s.sale_date) >= '2019-01-01'
+AND MAX(s.sale_date) <= '2019-03-31'
