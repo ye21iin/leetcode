a346720d554697309983afb28a800d91ce55f9f0
Yelin Kim <97957438+ye21iin@users.noreply.github.com>
2026-03-08 15:42:02 -0700

    Time: 440 ms (55.64%), Space: 0B (100%) - LeetHub

---
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,8 @@
+SELECT
+    sell_date,
+    COUNT(DISTINCT product) as num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product) as products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date
+;
