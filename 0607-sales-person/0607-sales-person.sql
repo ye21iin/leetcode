14e12fa42521529de7c6fd48154eb45fe99d93e6
Yelin Kim <97957438+ye21iin@users.noreply.github.com>
2026-03-18 22:42:50 -0700

    Time: 1358 ms (52.42%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,8 +1,9 @@
-SELECT name
-FROM SalesPerson
-WHERE sales_id NOT IN (
-  SELECT o.sales_id
+SELECT s.name
+FROM SalesPerson s
+WHERE NOT EXISTS (
+  SELECT 1
   FROM Orders o
   JOIN Company c ON o.com_id = c.com_id
-  WHERE c.name = 'RED'
+  WHERE o.sales_id = s.sales_id
+    AND c.name = 'RED'
 )
