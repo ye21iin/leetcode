82e6e42ed26f9c0b2b1fd40adfcf2849a3b3b1f8
Yelin Kim <97957438+ye21iin@users.noreply.github.com>
2026-03-30 19:21:51 -0700

    Time: 651 ms (86.49%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -2,7 +2,7 @@
 SELECT
     p.product_name, SUM(o.unit) AS unit
 FROM Orders o
-LEFT JOIN Products p ON o.product_id = p.product_id
+INNER JOIN Products p ON o.product_id = p.product_id
 WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
 GROUP BY o.product_id
 HAVING SUM(o.unit) >= 100
