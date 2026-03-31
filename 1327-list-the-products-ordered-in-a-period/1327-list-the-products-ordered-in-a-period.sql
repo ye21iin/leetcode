b433b0b2fef5044ed10ef016003872f175eb0276
Yelin Kim <97957438+ye21iin@users.noreply.github.com>
2026-03-30 18:53:19 -0700

    Time: 874 ms (13.3%), Space: 0B (100%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,9 @@
+# 2020년 2월에 최소 100개 이상 주문된 제품의 이름과 수량을 가져오는 솔루션을 작성하세요. 
+SELECT
+    p.product_name, SUM(o.unit) AS unit
+FROM Orders o
+LEFT JOIN Products p ON o.product_id = p.product_id
+WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY o.product_id
+HAVING SUM(o.unit) >= 100
+;
