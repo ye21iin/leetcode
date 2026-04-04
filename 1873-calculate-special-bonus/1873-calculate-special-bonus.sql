293b89c94601bcbd068dec880534d790b59ab9ba
Yelin Kim <97957438+ye21iin@users.noreply.github.com>
2026-04-04 15:41:44 -0700

    Time: 535 ms (90.03%), Space: 0B (100%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,7 +1,7 @@
 SELECT
     employee_id,
     CASE
-        WHEN employee_id % 2 != 0 AND NOT name RLIKE '^M'
+        WHEN employee_id % 2 <> 0 AND name NOT LIKE 'M%'
         THEN salary
         ELSE 0
     END as bonus
